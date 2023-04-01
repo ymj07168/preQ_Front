42a29bc30226ff3cab72d54eaeb1765809d7c16b
이효원 <ymj07168@gmail.com>
2023-04-02 00:25:55 +0900

    style: navbar 스타일 수정

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const NavBlock = styled.div`
+    height : 4rem;
+    display: fixed;
+    z-index : 1000;
+    margin-top : 30px;
+    margin-left: 100px;
+    margin-right: 100px;
+    align-items : center;
+    background : #afafaf;
+    border-radius: 30px;
+`
+
+const Menu = styled.div`
+    margin-left: auto;
+    margin-right: 100px;
+`
+
+const LinkTo = styled(Link)`
+    text-decoration: none;
+    color: black;
+    font-size: 15px;
+    margin-right : 20px;
+    margin-left : 20px;
+  &:hover{
+    text-decoration: none;
+    color: #ffffff;
+  }
+`
+
+
 
 const NavBar = () => {
     return (
         <>
-            <Link to='/' >Home</Link>
-            <Link to='/community'>Community</Link>
-            <Link to='/mypage'>MyPage</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/service'>PredictionService</Link>
+            <NavBlock>
+                <Menu>
+                    <LinkTo to='/' >Home</LinkTo>
+                    <LinkTo to='/community'>커뮤니티</LinkTo>
+                    <LinkTo to='/mypage'>마이페이지</LinkTo>
+                    <LinkTo to='/login'>로그인</LinkTo>
+                    <LinkTo to='/service'>질문예측서비스</LinkTo>
+                </Menu>
+            </NavBlock>
         </>
+
     )
 }
 
