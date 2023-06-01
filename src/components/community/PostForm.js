f62b6d70e620f63f71f194478b733a3f2ac1c654
이효원 <ymj07168@gmail.com>
2023-06-01 19:51:52 +0900

    feat: 게시글 수정 api 연동

--- src/components/community/PostForm.js
+++ src/components/community/PostForm.js
@@ -4,7 +4,8 @@ import InputBox from "../common/InputBox";
 import StyleButton from "../common/StyleButton";
 import { addPostItem } from "../../lib/api/community";
 import { getCookie } from "../../lib/cookie";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { editPostItem } from "../../lib/api/community";
 
 
 const PostFormContainer = styled.div`
@@ -38,12 +39,15 @@ const PostFormBox = styled.div`
 `
 
 
-const PostForm = ({ isEdit }) => {
+const PostForm = (props) => {
+
 
     const navigator = useNavigate();
 
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+    const { isEdit, id, preTitle, preContent } = props;
+
+    const [title, setTitle] = useState(isEdit ? preTitle : '');
+    const [content, setContent] = useState(isEdit ? preContent : '');
 
 
     // 게시글 생성
@@ -57,10 +61,26 @@ const PostForm = ({ isEdit }) => {
         await addPostItem(title, content, config)
             .then((res) => {
                 console.log(res);
-                navigator('/community');
+                const new_id = res.data.data.id;
+                navigator(`/community/item/${new_id}`);
             })
             .catch((err) => console.log(err));
+    }
 
+    // 게시글 수정
+    const onEditPostItem = async () => {
+        let config = {
+            headers: {
+                'Authorization': `Bearer ${getCookie('is_login')}`,
+                'withCredentials': true,
+            }
+        }
+        await editPostItem(id, title, content, config)
+            .then((res) => {
+                console.log(res);
+                navigator(`/community/item/${id}`);
+            })
+            .catch((err) => console.log(err));
     }
 
 
@@ -71,15 +91,15 @@ const PostForm = ({ isEdit }) => {
                     <div className="title-text">
                         제목
                     </div>
-                    <InputBox width="700px" height="60px" onChange={(e) => setTitle(e.target.value)} />
+                    <InputBox width="700px" height="60px" onChange={(e) => setTitle(e.target.value)} value={title} />
                     <br />
                     <div className="title-text">
                         본문
                     </div>
-                    <InputBox width="700px" height="500px" onChange={(e) => setContent(e.target.value)} />
+                    <InputBox width="700px" height="500px" onChange={(e) => setContent(e.target.value)} value={content} />
                     <div className="btn-box">
-                        <StyleButton width="140px" height="50px" size="20px" onClick={() => navigator('/community')}>취소</StyleButton>
-                        <StyleButton width="140px" height="50px" size="20px" onClick={onAddPostItem}>등록</StyleButton>
+                        <StyleButton width="140px" height="50px" size="20px" onClick={isEdit ? () => navigator(`/community/item/${id}`) : () => navigator('/community')}>취소</StyleButton>
+                        <StyleButton width="140px" height="50px" size="20px" onClick={isEdit ? onEditPostItem : onAddPostItem}>등록</StyleButton>
                     </div>
                 </PostFormBox>
             </PostFormContainer>
